347f60bb6bacb1e59cf3632d8527f4ee5676c1fe
kinsprite <qxqs11@126.com>
2017-04-03 16:30:19 +0800

    精简 stylelint

--- conf/stylelint-options.conf.js
+++ conf/stylelint-options.conf.js
@@ -4,22 +4,6 @@
 
 const configOverrides = {
   rules: {
-    'rscss/class-format': [
-      true,
-      {
-        // a-z0-9 - #{$()}
-        component: /^[a-z0-9\-#${}()]+$/,
-        element: /^[a-z0-9\-#${}()]+$/,
-        componentWhitelist: [
-          // These are Bootstrap classes that are typically extended as RSCSS components.
-          // These exceptions would be useful in projects that use RSCSS in Bootstrap sites.
-          'btn',
-          'container',
-          'checkbox',
-          'radio',
-        ],
-      },
-    ],
   },
 };
 
