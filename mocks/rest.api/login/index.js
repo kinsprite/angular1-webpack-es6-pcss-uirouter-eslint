7e6e5193785b97f6bdf47da5993ef910353bd4f7
kinsprite <qxqs11@126.com>
2017-06-21 00:11:51 +0800

    show request parameters

--- mocks/rest.api/login/index.js
+++ mocks/rest.api/login/index.js
@@ -6,16 +6,18 @@ const fs = require('fs');
 
 function loginGetService(request, content, callback) {
     console.log('Received headers:' + JSON.stringify(request.headers));
-    console.log('Received parameters :' + JSON.stringify(request.params));
+    // console.log('Received queries:' + JSON.stringify(request.query)); // query
+    console.log('Received parameters :' + JSON.stringify(request.parameters));
     console.log('Received JSON object:' + JSON.stringify(content)); // body
-    const userId = request.params.user !== undefined ? request.params.user : -1;
+    const userId = request.parameters.user !== undefined ? request.parameters.user : -1;
     return callback(null, fs.createReadStream(`./mocks/rest.api/login/login.${userId}.GET.json`, { encoding: 'utf-8' }));
 }
 
 function loginPutService(request, content, callback) {
     console.log('Received headers:' + JSON.stringify(request.headers));
-    console.log('Received parameters :' + JSON.stringify(request.params));
-    console.log('Received JSON object:' + JSON.stringify(request.body)); // body
+    // console.log('Received queries:' + JSON.stringify(request.query)); // query
+    console.log('Received parameters :' + JSON.stringify(request.parameters));
+    console.log('Received JSON object:' + JSON.stringify(content)); // body
     const userId = content.userId !== undefined ? content.userId : -1;
     return callback(null, fs.createReadStream(`./mocks/rest.api/login/login.${userId}.PUT.json`, { encoding: 'utf-8' }));
 }
